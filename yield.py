e9cad88f47d56bfb4fb9a9ba2ff016783eb72da7
HENA3196 <128054456+HENA3196@users.noreply.github.com>
2023-04-26 11:52:50 +0530

    Add files via upload

---
+++ yield.py
@@ -0,0 +1,24 @@
+
+def num():
+    yield 5
+
+value=num()
+print(value)
+
+
+# def gen():
+#     yield i
+
+# for i in gen(5)
+
+
+
+# def squares(n):
+#     for i in range(n):
+#         yield i ** 2
+
+# for num in squares(5):
+#     print(num)
+
+
+
